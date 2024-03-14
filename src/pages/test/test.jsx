72983d9369e5d4b7ed8a65326b326cb2993f9643
sangjin.heo <adsoulkey@g.hongik.ac.kr>
2024-03-14 10:16:25 +0900

    feat: 투자성향테스트 초안 구현

--- src/pages/test/test.jsx
+++ src/pages/test/test.jsx
@@ -1,16 +1,39 @@
-import React from "react";
-import Rank from "../../assets/backgrounds/rank.png";
+import React, { useState } from "react";
+import "./test.css";
+import character1 from "../../assets/backgrounds/character1.png";
+
 export default function TestLayout() {
+  const [startTest, setStartTest] = useState(false);
+
   return (
-    <div class="background-style">
-      <img src={Rank} style={{ width: "100%", height: "100%" }} />
-      <div
-        style={{
-          position: "absolute",
-          top: 100,
-          left: 100,
-        }}
-      ></div>
+    <div className="test-layout">
+      <img src={character1} className="character-image" />
+      <div className="test-content">
+        <div className="test-title">투자 성향 테스트</div>
+        <hr className="test-line" />
+        <div className="test-intro">
+          {!startTest && (
+            <div className="intro-text">
+              본인의 투자 성향을 알아볼 수 있는 간단한 테스트입니다.
+              <br />
+              문항은 총 7문항, 예상 소요시간은 3~5분입니다.
+            </div>
+          )}
+          {!startTest && (
+            <button
+              className="start-test-button"
+              onClick={() => setStartTest(true)}
+            >
+              테스트 하러 가기
+            </button>
+          )}
+        </div>
+        {startTest && (
+          <div className="test-questions">
+            여기에 오지선다 문항이 나타납니다.
+          </div>
+        )}
+      </div>
     </div>
   );
 }
