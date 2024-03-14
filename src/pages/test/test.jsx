1c0dbd7d844153e104a528a7b6db8ccc7096010d
sangjin.heo <adsoulkey@g.hongik.ac.kr>
2024-03-14 11:28:40 +0900

    feat: 투자성향 테스트 구현 중 중간 저장

--- src/pages/test/test.jsx
+++ src/pages/test/test.jsx
@@ -1,16 +1,60 @@
 import React, { useState } from "react";
 import "./test.css";
 import character1 from "../../assets/backgrounds/character1.png";
+import testData from "./testData";
 
 export default function TestLayout() {
   const [startTest, setStartTest] = useState(false);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+  // 진행 상태를 계산하는 함수
+  const calculateProgress = () => {
+    return ((currentQuestionIndex + 1) / testData.length) * 100;
+  };
+
+  const handleAnswerClick = () => {
+    if (currentQuestionIndex < testData.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    } else {
+      alert("테스트가 끝났습니다!");
+      setStartTest(false);
+      setCurrentQuestionIndex(0);
+    }
+  };
+
+  const renderOptions = () => {
+    const currentQuestion = testData[currentQuestionIndex];
+    return (
+      <div className="quiz-options">
+        <div className="question-text">{currentQuestion.question}</div>
+        {currentQuestion.answers.map((answer, index) => (
+          <button
+            key={index}
+            className="test-button"
+            onClick={handleAnswerClick}
+          >
+            {answer}
+          </button>
+        ))}
+      </div>
+    );
+  };
 
   return (
     <div className="test-layout">
-      <img src={character1} className="character-image" />
+      <img src={character1} className="character-image" alt="Character" />
       <div className="test-content">
         <div className="test-title">투자 성향 테스트</div>
         <hr className="test-line" />
+        {/* 진행 상태바를 추가 */}
+        {startTest && (
+          <div className="progress-bar-background">
+            <div
+              className="progress-bar-fill"
+              style={{ width: `${calculateProgress()}%` }}
+            ></div>
+          </div>
+        )}
         <div className="test-intro">
           {!startTest && (
             <div className="intro-text">
@@ -28,11 +72,7 @@ export default function TestLayout() {
             </button>
           )}
         </div>
-        {startTest && (
-          <div className="test-questions">
-            여기에 오지선다 문항이 나타납니다.
-          </div>
-        )}
+        {startTest && <div className="test-questions">{renderOptions()}</div>}
       </div>
     </div>
   );
