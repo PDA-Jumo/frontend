0406cc1e12e394c4c7d2a27fac2357f8aecf794b
sangjin.heo <adsoulkey@g.hongik.ac.kr>
2024-03-19 10:27:40 +0900

    Fix: CSS 수정 중간

--- src/pages/user/signin/page.jsx
+++ src/pages/user/signin/page.jsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./page.css";
+import "./page.css"; // CSS 파일을 임포트합니다.
 import { login } from "../../../lib/apis/auth";
 import useAuth from "../../../lib/hooks/useAuth";
 import { useDispatch } from "react-redux";
@@ -19,77 +19,76 @@ export default function SignInPage() {
 
   const onSubmitLogin = useCallback(
     async (email, password) => {
-      // async 키워드를 추가하여 비동기 함수임을 명시합니다.
       try {
-        const resp = await login({ email, password }); // await을 사용하여 비동기 로그인 함수의 완료를 기다립니다.
+        const resp = await login({ email, password });
         const userData = resp.data;
 
         if (userData) {
-          await clientLogin(userData); // 클라이언트 측 로그인 처리
+          await clientLogin(userData);
           const action = loginAction(userData);
           console.log(action);
           dispatch(action);
-          navigate("/home"); // 모든 처리가 완료된 후 홈으로 이동
+          navigate("/home");
         }
       } catch (error) {
-        console.error(error); // 에러 처리
-        // 필요하다면 에러에 대한 추가적인 처리를 여기에 작성할 수 있습니다.
+        console.error(error);
       }
     },
-    [dispatch, navigate, clientLogin] // 의존성 배열에 포함된 항목들
+    [dispatch, navigate, clientLogin]
   );
 
   const onHandleSignUpClick = () => {
     navigate("/signup");
   };
-
   return (
-    <div className="container">
-      <div style={{ width: "100%", maxWidth: "640px" }}>
-        <h3 style={{ alignSelf: "start" }}>Login</h3>
-        <div className="form-floating mb-3">
-          <input
-            type="email"
-            className="form-control"
-            id="floatingInput"
-            placeholder="name@example.com"
-            onChange={(e) => {
-              onInputChange(e.target.value, setUserEmail);
-            }}
-            value={userEmail}
-            required
-          />
-          <label htmlFor="floatingInput">Email address</label>
+    <div className="centered-container">
+      <div className="rounded-rectangle">
+        <div className="form-group">
+          {" "}
+          {/* form-group 추가 */}
+          <div className="form-floating mb-3">
+            <input
+              type="email"
+              className="form-control custom-input" // 클래스 이름에 custom-input 추가
+              id="floatingInput"
+              placeholder="name@example.com"
+              onChange={(e) => {
+                onInputChange(e.target.value, setUserEmail);
+              }}
+              value={userEmail}
+              required
+            />
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              type="password"
+              className="form-control custom-input" // 클래스 이름에 custom-input 추가
+              id="floatingPassword"
+              placeholder="Password"
+              onChange={(e) => {
+                onInputChange(e.target.value, setUserPassword);
+              }}
+              value={userPassword}
+              required
+            />
+          </div>
+          <div className="button-group">
+            {" "}
+            {/* 버튼 그룹을 위한 div 추가 */}
+            <button
+              className="btn btn-primary"
+              onClick={(e) => {
+                e.preventDefault();
+                onSubmitLogin(userEmail, userPassword);
+              }}
+            >
+              로그인
+            </button>
+            <button className="btn btn-secondary" onClick={onHandleSignUpClick}>
+              회원가입
+            </button>
+          </div>
         </div>
-        <div className="form-floating mb-3">
-          <input
-            type="password"
-            className="form-control"
-            id="floatingPassword"
-            placeholder="Password"
-            onChange={(e) => {
-              onInputChange(e.target.value, setUserPassword);
-            }}
-            value={userPassword}
-            required
-          />
-          <label htmlFor="floatingPassword">Password</label>
-        </div>
-        <button
-          className="w-100 btn btn-primary"
-          onClick={(e) => {
-            e.preventDefault();
-            onSubmitLogin(userEmail, userPassword);
-          }}
-        >
-          로그인
-        </button>
-        <button
-          className="w-100 btn btn-secondary mt-3"
-          onClick={onHandleSignUpClick}
-        >
-          회원가입
-        </button>
       </div>
     </div>
   );
