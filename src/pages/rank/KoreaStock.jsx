fec7b2b1f5149c091cf87103e6cde5f29926b266
sangjin.heo <adsoulkey@g.hongik.ac.kr>
2024-03-21 16:36:56 +0900

    Feat: Hover 기능 구현

--- src/pages/rank/KoreaStock.jsx
+++ src/pages/rank/KoreaStock.jsx
@@ -4,16 +4,20 @@ import Coin from "../../assets/stock/coin.png";
 import Chart from "../../assets/stock/Increase.png";
 import Folder from "../../assets/stock/folder.png";
 import Arrow from "../../assets/stock/arrow.png";
-// API 호출 대신 더미 데이터를 사용합니다.
-// import { getKoreaPortfolio } from "../../lib/apis/portfolio";
 import { PieChartComponent } from "./PieChart.js";
 
 export default function KoreaStock() {
   const [myStock, setMyStock] = useState([]);
   const [assets, setAssets] = useState("0");
-  const [chart, setChart] = useState({});
+  const [chart, setChart] = useState([]);
   const [hoverdata, setHoverdata] = useState("");
+  function handleMouseEnter(stockName) {
+    setHoverdata(stockName);
+  }
 
+  function handleMouseLeave() {
+    setHoverdata("");
+  }
   useEffect(() => {
     const setData = async () => {
       const resp = {
@@ -21,7 +25,7 @@ export default function KoreaStock() {
           "삼성전자",
           "LG화학",
           "현대자동차",
-          "LG에너지솔루션",
+          "KT&G",
           "오브젠",
           "프로티아",
         ],
@@ -30,7 +34,7 @@ export default function KoreaStock() {
           { stock_name: "삼성전자", percent: 10 },
           { stock_name: "LG화학", percent: 20 },
           { stock_name: "현대자동차", percent: 10 },
-          { stock_name: "LG에너지솔루션", percent: 10 },
+          { stock_name: "KT&G", percent: 10 },
           { stock_name: "오브젠", percent: 20 },
           { stock_name: "프로티아", percent: 20 },
         ],
@@ -66,28 +70,20 @@ export default function KoreaStock() {
           <div className="mediumText text-white">{assets}원</div>
           <div>
             {chart && chart.length > 0 && (
-              <PieChartComponent codeRatioArray={chart} onHover={handleHover} />
+              <PieChartComponent
+                codeRatioArray={chart}
+                onHover={handleHover}
+                hoverdata={hoverdata}
+              />
             )}
           </div>
-          {/*<div className="profitInfo">
-            <div className="totalReturn">
-              <div className="text-white">전체 수익률</div>
-              <div className="profit">19.37%</div>
-            </div>
-            <div className="evaluationProfit">
-              <div className="text-white">평가 수익금액</div>
-              <div className="profit">7,040,204</div>
-            </div>
-          </div>
-            */}{" "}
-          {/* 이 부분 피그마에 없어서 일단 주석처리함 */}
         </div>
       </div>
 
       <div className="rightSection">
         <div>
           <div className="largeText text-white stockDisplay">
-            <img src={Chart} className="icon" />
+            <img src={Chart} className="icon" alt="Increase" />
             국내 보유종목
           </div>
         </div>
@@ -99,6 +95,8 @@ export default function KoreaStock() {
               <div
                 key={id}
                 className={`stockItem ${isHovered ? "hovered" : ""}`}
+                onMouseEnter={() => handleMouseEnter(stock)}
+                onMouseLeave={handleMouseLeave}
               >
                 <div style={{ display: "flex", alignItems: "center" }}>
                   <img
--- src/pages/rank/PieChart.js
+++ src/pages/rank/PieChart.js
@@ -3,21 +3,18 @@ import { PieChart, Pie, Cell } from "recharts";
 import * as PieCharts from "../../styles/PieChart.js";
 import { popularColors } from "./colorSelector.js";
 
-
 function getRandomColor() {
-    const randomIndex = Math.floor(Math.random() * popularColors.length);
-    return popularColors[randomIndex];
+  const randomIndex = Math.floor(Math.random() * popularColors.length);
+  return popularColors[randomIndex];
 }
 
-export function PieChartComponent({ codeRatioArray, onHover }) {
+export function PieChartComponent({ codeRatioArray, onHover, hoverdata }) {
   const [colors, setColors] = useState([]);
 
   useEffect(() => {
     const generatedColors = codeRatioArray.map(() => getRandomColor());
     setColors(generatedColors);
-  }, [codeRatioArray]); 
-
-  console.log(codeRatioArray);
+  }, [codeRatioArray]);
 
   return (
     <PieCharts.Container style={{ marginLeft: '50px', marginTop: '20px' }}>
@@ -28,13 +25,13 @@ export function PieChartComponent({ codeRatioArray, onHover }) {
           cy="50%"
           innerRadius={80}
           outerRadius={100}
-          dataKey="percent" // 데이터 키는 실제 사용하는 데이터에 맞게 조정하세요.
+          dataKey="percent"
           isAnimationActive={false}
         >
           {codeRatioArray.map((entry, index) => (
             <Cell
               key={`cell-${index}`}
-              fill={colors[index] || getRandomColor()} // colors 배열에서 색상을 가져오거나, 없으면 새로 생성
+              fill={colors[index] || getRandomColor()} 
               onMouseOver={() => onHover(entry)}
             />
           ))}
