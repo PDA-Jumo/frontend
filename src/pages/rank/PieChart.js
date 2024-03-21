6afecd67f1bbe241eb77ed55cac30d61cafed222
sangjin.heo <adsoulkey@g.hongik.ac.kr>
2024-03-21 15:23:20 +0900

    Feat: 색상 랜덤 생성 기능 및 종합적인 포트폴리오 컴포넌트 구현

--- src/pages/rank/PieChart.js
+++ src/pages/rank/PieChart.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { PieChart, Pie, Cell } from "recharts";
 import * as PieCharts from "../../styles/PieChart.js";
+import { popularColors } from "./colorSelector.js";
+
+
+function getRandomColor() {
+    const randomIndex = Math.floor(Math.random() * popularColors.length);
+    return popularColors[randomIndex];
+}
 
 export function PieChartComponent({ codeRatioArray, onHover }) {
+  const [colors, setColors] = useState([]);
+
+  useEffect(() => {
+    const generatedColors = codeRatioArray.map(() => getRandomColor());
+    setColors(generatedColors);
+  }, [codeRatioArray]); 
+
   console.log(codeRatioArray);
-  const COLORS = ["#D2E0FB", "#F9F3CC", "#D7E5CA", "#8EACCD"]; // 직접 설정한 색상
 
   return (
-    <PieCharts.Container style={{ marginLeft: '50px' , marginTop: '20px'}}>
+    <PieCharts.Container style={{ marginLeft: '50px', marginTop: '20px' }}>
       <PieChart width={200} height={200}>
         <Pie
           data={codeRatioArray}
@@ -15,14 +28,13 @@ export function PieChartComponent({ codeRatioArray, onHover }) {
           cy="50%"
           innerRadius={80}
           outerRadius={100}
-          //dataKey="stock_assets"
-          dataKey="percent" // API 달면 윗줄 코드로 바꿔줘야함
+          dataKey="percent" // 데이터 키는 실제 사용하는 데이터에 맞게 조정하세요.
           isAnimationActive={false}
         >
           {codeRatioArray.map((entry, index) => (
             <Cell
               key={`cell-${index}`}
-              fill={COLORS[index % COLORS.length]}
+              fill={colors[index] || getRandomColor()} // colors 배열에서 색상을 가져오거나, 없으면 새로 생성
               onMouseOver={() => onHover(entry)}
             />
           ))}
