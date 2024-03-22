92ca715ccf7e9d788c207dc1c3eea5c90bc97214
baebyeolha <qufgk0207@naver.com>
2024-03-22 09:09:11 +0900

    Feat: 커뮤니티 검색 기능 추가

--- src/lib/apis/community.js
+++ src/lib/apis/community.js
@@ -3,5 +3,6 @@ import instance from "./base";
 export const getSearchRoom = async (keyword) => {
   const res = await instance.get(`community/search?stock_name=${keyword}`);
   console.log(res.data);
+  console.log("쉬벌");
   return res.data;
 };
