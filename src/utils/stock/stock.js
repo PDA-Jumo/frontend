f809830906944eb278182f765650c2380eab2243
yeri <rlafl7942@naver.com>
2024-03-22 19:11:10 +0900

    Fix: 실시간 인기 종목 랭킹 socket 제거

--- src/utils/stock/stock.js
+++ src/utils/stock/stock.js
@@ -9,3 +9,18 @@ export const getMarketIssue = async () => {
   });
   console.log(marketIssues);
 };
+
+export const getLiveRanking = async (type) => {
+  const rankingData = await axios.get(
+    "/stock/liveRanking",
+    {
+      headers: {
+        apiKey: process.env.REACT_APP_SHINHAN_API_KEY,
+      },
+    },
+    {
+      type: type,
+    }
+  );
+  console.log(rankingData);
+};
