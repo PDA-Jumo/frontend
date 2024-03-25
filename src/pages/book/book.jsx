0e8640e59725532107db2d8aa6815ff75b61b4b2
sangjin.heo <adsoulkey@g.hongik.ac.kr>
2024-03-25 10:06:42 +0900

    Feat: 도감 1차 완성

--- src/pages/book/book.jsx
+++ src/pages/book/book.jsx
@@ -3,6 +3,7 @@ import Book from "../../assets/backgrounds/Book.png";
 import { useNavigate } from "react-router-dom";
 import "./book.css";
 import { data } from "./bookdata.js";
+import { useSelector, useDispatch } from "react-redux";
 
 const levels = [
   "Lv.0 주탄생",
@@ -19,15 +20,30 @@ const levels = [
 export default function BookLayout() {
   const navigate = useNavigate();
   const [selectedLevelData, setSelectedLevelData] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [selectedLevel, setSelectedLevel] = useState("");
+
+  const user = useSelector((state) => state.user.user) || {};
+
+  console.log(user?.level);
 
   const handleBack = () => {
-    console.log("뒤로가기 버튼이 클릭되었습니다.");
-    navigate("/home");
+    if (selectedItem) {
+      setSelectedItem(null);
+    } else {
+      navigate("/home");
+    }
   };
 
   const handleLevelClick = (level) => {
     const filteredData = data.filter((item) => item.level === level);
     setSelectedLevelData(filteredData);
+    setSelectedItem(null);
+    setSelectedLevel(level);
+  };
+
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
   };
 
   return (
@@ -37,27 +53,58 @@ export default function BookLayout() {
         className="BookLayout-backgroundImage"
         alt="Book Background"
       />
-      <div className="BookLayout-content">
-        <div className="BookLayout-rectangle">
-          <div className="BookLayout-text">레벨을 선택해보세요!</div>
-          <ul className="BookLayout-list">
-            {levels.map((level, index) => (
-              <li key={index} onClick={() => handleLevelClick(level)}>
-                {level}
-              </li>
-            ))}
-          </ul>
-        </div>
-        {selectedLevelData.length > 0 && (
-          <div className="BookLayout-grid">
-            {selectedLevelData.map((item, index) => (
-              <div key={index} className="BookLayout-gridItem">
-                <div>{item.title}</div>
-              </div>
-            ))}
+      {!selectedItem && (
+        <div className="BookLayout-content">
+          <div className="BookLayout-rectangle">
+            <div className="BookLayout-text">레벨을 선택해보세요!</div>
+            <ul className="BookLayout-list">
+              {levels.map((level, index) => {
+                const levelNum = parseInt(level.match(/\d+/)[0]);
+
+                console.log(levelNum);
+                const isDisabled = user?.level < levelNum;
+                return (
+                  <li
+                    key={index}
+                    className={isDisabled ? "BookLayout-listItem-disabled" : ""}
+                    onClick={() => !isDisabled && handleLevelClick(level)}
+                  >
+                    {level}
+                    {selectedLevel === level && " →"}
+                  </li>
+                );
+              })}
+            </ul>
           </div>
-        )}
-      </div>
+          {selectedLevelData.length > 0 && (
+            <div className="BookLayout-grid">
+              {selectedLevelData.map((item, index) => (
+                <div
+                  key={index}
+                  className="BookLayout-gridItem"
+                  onClick={() => handleItemClick(item)}
+                >
+                  <div>{item.title}</div>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      )}
+      {selectedItem && (
+        <div className="BookLayout-content">
+          <div className="BookLayout-detail">
+            <h1>
+              {selectedLevel} → {selectedItem.title}
+            </h1>
+            <div className="BookLayout-itemWrapper">
+              <h1 className="BookLayout-itemTitle">{selectedItem.title}</h1>
+              <p className="BookLayout-itemBody">{selectedItem.body}</p>
+            </div>
+          </div>
+        </div>
+      )}
+
       <button className="room-back-button" onClick={handleBack}>
         뒤로가기
       </button>
