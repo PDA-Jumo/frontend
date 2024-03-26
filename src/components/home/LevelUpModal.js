7b17f94ba77c6fbadaa866f14266a12c6139adc7
yeri <rlafl7942@naver.com>
2024-03-26 16:35:31 +0900

    Feat: 레벨업 모달 레벨 별 문구 적용

--- src/components/home/LevelUpModal.js
+++ src/components/home/LevelUpModal.js
@@ -1,10 +1,158 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import styled, { keyframes } from "styled-components";
 
 // icons
 import up from "../../assets/main/up.png";
 
 export default function LevelUpModal(props) {
+  const level = useSelector((state) => state.user.user.level);
+  const contents = [
+    {
+      levelName: "주얼딩",
+      reward: (
+        <>
+          보상으로{" "}
+          <span style={{ color: " #FF5656" }}>[ 투자 지원금 100,000원 ]</span>을
+          드립니다.
+        </>
+      ),
+      available: (
+        <>
+          이번 레벨부터는{" "}
+          <span style={{ color: " #FF5656" }}>[ 한 종목 주식 거래 ]</span>가
+          가능합니다.
+        </>
+      ),
+    },
+    {
+      levelName: "주린이",
+      reward: (
+        <>
+          보상으로{" "}
+          <span style={{ color: " #FF5656" }}>[ 코스피 200 중 한 종목 ]</span>을
+          드립니다.
+        </>
+      ),
+      available: (
+        <>
+          이번 레벨부터는{" "}
+          <span style={{ color: " #FF5656" }}>
+            [ 실시간 인기테마, 뉴스 조회 ]
+          </span>
+          가 가능합니다.
+        </>
+      ),
+    },
+    {
+      levelName: "주초딩",
+      reward: (
+        <>
+          보상으로{" "}
+          <span style={{ color: " #FF5656" }}>[ 투자지원금 10만원 ]</span>을
+          드립니다.
+        </>
+      ),
+      available: (
+        <>
+          <span style={{ color: " #FF5656" }}>국내 주식 거래 가능</span>{" "}
+          레벨까지 한 레벨 남았습니다!
+        </>
+      ),
+    },
+    {
+      levelName: "주중딩",
+      reward: (
+        <>
+          <span style={{ color: " #FF5656" }}>
+            [ 퀴즈 보상 5000원 업그레이드, UI 강화 ]
+          </span>
+          보상을 드립니다.
+        </>
+      ),
+      available: (
+        <>
+          이번 레벨부터는{" "}
+          <span style={{ color: " #FF5656" }}>
+            [ 국내 주식 / ETF 거래와 랭킹 조회 ]
+          </span>
+          가 가능합니다.
+        </>
+      ),
+    },
+    {
+      levelName: "주고딩",
+      reward: (
+        <>
+          <span style={{ color: " #FF5656" }}>
+            [ 투자 성향 진단 기회와 퀴즈 보상 10000원 업그레이드 ]
+          </span>
+          보상을 드립니다.
+        </>
+      ),
+
+      available: (
+        <>
+          이번 레벨부터는{" "}
+          <span style={{ color: " #FF5656" }}>
+            [ 매도매수 탭 주식 추천 조회 ]
+          </span>
+          가가능합니다.
+        </>
+      ),
+    },
+    {
+      levelName: "주대딩",
+      reward: (
+        <>
+          <span style={{ color: " #FF5656" }}>
+            [ 퀴즈 보상 50,000원 업그레이드, UI 강화, 해외 주식 랜덤 1주 ]
+          </span>{" "}
+          보상을 드립니다.
+        </>
+      ),
+      available: (
+        <>
+          이번 레벨부터는{" "}
+          <span style={{ color: " #FF5656" }}>[ 해외 주식 거래 ]</span>
+          가가능합니다.
+        </>
+      ),
+    },
+    {
+      levelName: "주졸부",
+      reward: (
+        <>
+          보상으로{" "}
+          <span style={{ color: " #FF5656" }}>
+            [ UI 강화, 커뮤니티 입장권 ]
+          </span>
+          을 드립니다. `
+        </>
+      ),
+      available: (
+        <>
+          <span style={{ color: " #FF5656" }}>마지막 레벨</span>까지 한 단계
+          남았습니다!
+        </>
+      ),
+    },
+    {
+      levelName: "주대주주",
+      reward: (
+        <>
+          보상으로 <span style={{ color: " #FF5656" }}>[ UI 강화 ]</span>를
+          드립니다.
+        </>
+      ),
+      available: (
+        <>
+          <span style={{ color: " #FF5656" }}>마지막 레벨</span>에 도달했습니다.
+          축하드립니다!
+        </>
+      ),
+    },
+  ];
   return (
     <div
       style={{
@@ -52,19 +200,13 @@ export default function LevelUpModal(props) {
         </div>
         <div style={{ width: "100%", fontSize: "32px" }}>
           <p>
-            <span style={{ color: "#F3322C" }}>[ 주얼딩 ]</span>으로 레벨업
-            하셨습니다!
+            <span style={{ color: "#F3322C" }}>
+              {contents[level].levelName}
+            </span>
+            으로 레벨업 하셨습니다!
           </p>
-          <p>
-            보상으로{" "}
-            <span style={{ color: " #FF5656" }}>[ 투자 지원금 100,000원 ]</span>
-            을 드립니다.
-          </p>
-          <p>
-            이번 레벨부터는{" "}
-            <span style={{ color: " #FF5656" }}>[ 주식 1종목 거래 ]</span>가
-            가능합니다.
-          </p>
+          <p>{contents[level].reward}</p>
+          <p>{contents[level].available}</p>
         </div>
         <div
           style={{
