b7732870541c36fb70f34553b08cee3d0b4c2d89
sangjin.heo <adsoulkey@g.hongik.ac.kr>
2024-03-26 16:01:16 +0900

    Feat: state가 아닌 api 형식으로 갱신하도록 수정

--- src/pages/rank/layout.jsx
+++ src/pages/rank/layout.jsx
@@ -102,7 +102,9 @@ export default function RankLayout() {
         {currentUser.user_id !== user.user_id && (
           <button
             className="portfolio-button"
-            onClick={() => navigate(`/ranking/${user.user_id}`)}
+            onClick={() => {
+              navigate(`/ranking/${user.user_id}`, {});
+            }}
           >
             포트폴리오 보기
           </button>
--- src/pages/rank/rankPortfolio.jsx
+++ src/pages/rank/rankPortfolio.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 import Rank from "../../assets/backgrounds/rank.png";
 import Coin from "../../assets/coin.png";
 import KoreaStock from "./KoreaStock";
@@ -7,29 +8,53 @@ import WorldStock from "./WorldStock";
 import "./rank.css";
 import "./portfolio.css";
 import "./stockPortfolio.css";
+import levelData from "../home/levelData";
 
 export default function PortfolioList() {
   const navigate = useNavigate();
+  const { userId } = useParams();
   const [selectedTab, setSelectedTab] = useState("");
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const response = await axios.get(`/users/updateUsers/${userId}`);
+        setUserInfo({
+          level: response.data[0].level,
+          nickname: response.data[0].nickname,
+          total_assets: response.data[0].total_assets,
+        });
+      } catch (error) {
+        console.error("사용자 정보를 가져오는 데 실패했습니다.", error);
+      }
+    };
+
+    fetchUserInfo();
+  }, [userId, selectedTab]);
+
   return (
     <div className="background-style">
       <img src={Rank} className="background-image" alt="Rank Background" />
       <div className="ranking-header-container">
         <div className="ranking-header text-white">포트폴리오</div>
       </div>
-      {/* TODO ranking/:userId로 navigate 되었으니까, pp-title, 닉네임, 금액을 갱신해와야 함*/}
       <div className="pp-position">
         {selectedTab === "" && (
           <>
             <div className="ranking-content">
               <div>
-                <div className="pp-title">주대주주</div>
-                <div className="text-white text-bigtitle">김광태가뭐야</div>
+                <div className="pp-title">{levelData[userInfo?.level]} </div>
+                <div className="text-white text-bigtitle">
+                  {userInfo?.nickname || "로딩 중..."}
+                </div>
                 <div className="asset-container">
                   <div className="text-white text-title">
                     <img src={Coin} className="coin-image" alt="" />총 보유자산
                   </div>
-                  <div className="text-white text-title">1,000,000,000원</div>
+                  <div className="text-white text-title">
+                    {userInfo?.total_assets}원
+                  </div>
                 </div>
               </div>
             </div>
@@ -53,7 +78,7 @@ export default function PortfolioList() {
         {selectedTab === "world" && <WorldStock />}
       </div>
 
-      {selectedTab == "" && (
+      {selectedTab === "" && (
         <button className="back-button1" onClick={() => navigate("/ranking")}>
           뒤로가기
         </button>
