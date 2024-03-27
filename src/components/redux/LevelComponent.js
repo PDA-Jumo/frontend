9d3b8c3a08bdd47512806d2fdb5733ae02d9dcf2
yeri <rlafl7942@naver.com>
2024-03-27 14:49:09 +0900

    Feat: 레벨업 로직 수정

--- src/components/redux/LevelComponent.js
+++ src/components/redux/LevelComponent.js
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { getMyStockList, levelUp } from "../../lib/apis/level";
 import {
   updateFinancialsAction,
   updateLevelAction,
@@ -9,14 +10,21 @@ import LevelUpModal from "../home/LevelUpModal";
 export default function LevelComponent() {
   const user = useSelector((state) => state.user.user);
   const [isLevelUp, setIsLevelUp] = useState(false);
+  const [myStockLen, setMyStockLen] = useState(0);
   const dispatch = useDispatch();
 
-  const openByLevel = (bonus, newLevel) => {
-    dispatch(updateFinancialsAction(bonus));
+  const openByLevel = async (bonus, newLevel) => {
+    try {
+      const resp = await levelUp(bonus, user.user_id);
+      const data = resp.data;
 
-    if (!user.check[user.level]) {
-      setIsLevelUp(true);
-      dispatch(updateLevelAction(newLevel));
+      if (data === "성공") {
+        dispatch(updateFinancialsAction(bonus));
+        dispatch(updateLevelAction(newLevel));
+        setIsLevelUp(true);
+      }
+    } catch (error) {
+      console.error(error);
     }
   };
 
@@ -24,37 +32,41 @@ export default function LevelComponent() {
     if (user.cash >= 10000 && user.level === 0) {
       // level 0->1
       openByLevel(100000, 1);
-      // 한 종목 주식 거래 가능
-    } else if (user.level === 1) {
-      // 한 종목 보유 Level 1->2
-      openByLevel(0, 2); //투자지원금 없음
-      // 코스피 200 중 1주 랜덤
-      // 실시간 인기테마, 뉴스 (정보제공)
+      // 한 종목 주식 거래 가능 -> 매수매도 아이콘 블러 처리 해야함
+    } else if (user.level === 1 && myStockLen >= 1) {
+      // Level 1->2
+      openByLevel(0, 2);
+      // 코스피 200 중 1주 랜덤 지급 해야함
     } else if (user.level === 2 && user.cash >= 200000) {
       //Level 2->3
       openByLevel(100000, 3);
-    } else if (user.level === 3 && user.cash >= 400000) {
-      // 3종류 이상의 주식으로 포트폴리오 구성, 40만원 달성
+    } else if (user.level === 3 && user.cash >= 400000 && myStockLen >= 3) {
       // Level 3->4
-      // 퀴즈 단가 인상
+      openByLevel(0, 4);
     } else if (user.level === 4 && user.cash >= 1000000) {
       // Level 4->5
-      // 퀴즈 단가 인상
-      // 투자 성향 테스트
+      // 투자 성향 테스트 ok
+      openByLevel(0, 5);
     } else if (user.level === 5 && user.cash >= 10000000) {
       // Level 5->6
-      // 퀴즈 단가 인상
-      // 해외주식 랜덤 1주
+      // 해외주식 랜덤 1주 지급 해야함
+      openByLevel(0, 6);
     } else if (user.level === 6 && user.cash >= 50000000) {
       // Level 6->7
-      // 커뮤니티 입장 가능
+      openByLevel(0, 7);
     } else if (user.level === 7) {
       // 랭킹 중 상위 5% Level 7->8
+      openByLevel(0, 8);
     }
-  }, [user.cash]);
+  }, [user.cash, myStockLen]);
+
+  const getMyStockAsyncFun = async () => {
+    const myStockData = await getMyStockList(user.user_id);
+    setMyStockLen(myStockData.data.length);
+  };
 
   useEffect(() => {
-    console.log(user.level);
+    getMyStockAsyncFun();
   }, []);
 
   return <>{isLevelUp ? <LevelUpModal setIsLevelUp={setIsLevelUp} /> : null}</>;
