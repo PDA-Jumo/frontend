1de844efbdfede19d6b4ee109a160ac40a16c73d
seohee99 <imshee99@gmail.com>
2024-03-28 16:01:06 +0900

    Feat: redux test 브랜치 합침

--- src/lib/apis/stock.js
+++ src/lib/apis/stock.js
@@ -25,6 +25,16 @@ export const getThemeRank = async () => {
   return themeRank;
 };
 
+export const getLiveRanking = async (type) => {
+  const apiKey = process.env.REACT_APP_SHINHAN_API_KEY;
+  const liveRanking = await instance.get(`/stock/liveRanking/${type}`, {
+    headers: {
+      apiKey: apiKey,
+    },
+  });
+  return liveRanking;
+};
+
 export const getStockDetail = async (code, name) => {
   const resp = await instance.get(`/stock/detail/${code}/${name}`);
   return resp.data;
