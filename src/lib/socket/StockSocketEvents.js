d7c31798468947eea4cb3b3264ecdf96a381d330
seohee99 <imshee99@gmail.com>
2024-03-31 14:51:12 +0900

    Feat: 메인 화면 현재가 불러오는 socket event 추가

--- src/lib/socket/StockSocketEvents.js
+++ src/lib/socket/StockSocketEvents.js
@@ -9,15 +9,21 @@ const getStockdata = (callback) => {
   stockSocket.on("stock_update", callback);
 };
 
+const getStockPrice = (stock_code) => {
+  stockSocket.on(`current_${stock_code}`, { stock_code });
+};
+
 const leaveRoom = (stock_code, user_id) => {
   stockSocket.emit("leaveRoom", { stock_code, user_id });
   console.log(`${stock_code}방에서 ${user_id}님이 퇴장하셨습니다.`);
 
-  stockSocket.off("currentStockData");
+  stockSocket.off("stock_update");
+  stockSocket.off(`current_${stock_code}`);
 };
 
 export default {
   joinRoom,
   getStockdata,
+  getStockPrice,
   leaveRoom,
 };
